61170f0aa423647a6ef634f79dab7aa60c19ebd5
julien.thai <julien.thai@gmail.com>
2014-05-22 07:40:36 +0200

    add filters for pagination

---
+++ src/main/webapp/js/custom/filters.js
@@ -0,0 +1,9 @@
+angular.module('melmFilters', []).filter('startFrom', function() {
+  return function(input, start) {
+    if (input === undefined) {
+      return input;
+    } else {
+      return input.slice(+start);
+    }
+  };
+});
