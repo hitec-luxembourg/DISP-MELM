5eced1c98d5c90efbf3bf6f7593399996c245fb9
bmichiels <benoit.michiels@gmail.com>
2014-06-04 11:51:40 +0200

    [PSSU/Trac] #3162: Implement a preview of icon when adding or updating icon

--- src/main/webapp/js/custom/directives.js
+++ src/main/webapp/js/custom/directives.js
@@ -58,3 +58,19 @@ app.directive('ngThumb', [ '$window', function($window) {
     }
   };
 } ]);
+
+/**
+ * Ng File Select directive
+ */
+app.directive("ngFileSelect", function() {
+  return {
+    link : function($scope, el) {
+
+      el.bind("change", function(e) {
+
+        $scope.file = (e.srcElement || e.target).files[0];
+        $scope.getFile(e.target.id);
+      });
+    }
+  };
+});
